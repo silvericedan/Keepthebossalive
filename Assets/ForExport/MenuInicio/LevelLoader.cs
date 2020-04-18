9ea463767fe327cc15145c6db575fe0c6bfafe46
Hermosilla Reinaldo <ghostcad@gmail.com>
2020-04-18 14:45:33 -0300

    Agregado escena inicial

---
+++ Assets/ForExport/MenuInicio/LevelLoader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+    // referenciamos un Slider para la barra de Loading
+    public Slider sliderLoading;
+
+    // Funcion simple que llama a una corutina con la variable sceneIndex seteada publica
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+
+    // Corutina para cargar la escena enumerada de forma paralela sin congelar la escena actual.
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        operation.allowSceneActivation = false;
+
+        while (!operation.isDone)
+        {
+            sliderLoading.value = operation.progress;
+            Debug.Log(operation.progress);
+            if (sliderLoading.value == 0.9f)
+            {
+                sliderLoading.value = 1f;
+                operation.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+
+    // Funcion publica que luego llamamos con el boton de Exit para salir del juego.
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game!!");
+    }
+}
---
+++ Assets/ForExport/MenuInicio/SettingsMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    // Referenciamos una clase AudioMixer para poner controlar su volumen a travez de la funcion
+    // el valor float lo introducimos cuando llamamos la funcion moviendo el slider
+    public AudioMixer audioMixer;
+    
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volumeParam", volume);
+    }
+}
