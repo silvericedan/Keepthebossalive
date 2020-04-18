3c614a519b06aefadfd7d45254370d6774a12ec4
Gerardo Herrera <gerardoherrera87@yahoo.com.ar>
2020-04-18 14:47:51 -0300

    Scene map sprite script prefabs added

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance = null;
+    public int score;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        if(instance != this)
+        {
+            Destroy(gameObject);
+            DontDestroyOnLoad(gameObject);
+            
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
